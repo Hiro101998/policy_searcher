9817767b713621ffab700fcf1dee9b0c0d1d53cf
Hiro101998 <asameshi0319@gmail.com>
2021-07-27 12:41:54 +0900

    リロード時のnotfoundを修正

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,7 +5,7 @@ import SearchResult from './views/searchResult.vue'
 import Favorite from './views/Favorite.vue'
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
     {
       path: '/home',
