9cfb1adb34b2e87deec7e49e029cefd634445961
Hiro101998 <asameshi0319@gmail.com>
2021-08-22 15:32:20 +0900

    favoriteの読み込みが長い問題を解決

--- routes/api.php
+++ routes/api.php
@@ -52,16 +52,13 @@ Route::post('/store',[App\Http\Controllers\FavoriteController::class,'store'])->
 Route::post('/delete',[App\Http\Controllers\FavoriteController::class,'delete'])->name('delete');
 
 //favoriteにお気に入りに追加しているデータを送る。
-Route::get('/favorite',function (Request $request) {
+Route::get('/favorite/{id}',function ($id) {
 	//必要なデータをfavorite.vueに送る
-	$universities = \DB::table('universities')->join('departments','universities.university_id','=','departments.university_id')->join('subjects','departments.department_id','=','subjects.department_id')
-    ->join('policies','subjects.subject_id','=','policies.subject_id')->get();
-	$favorites = \DB::table('favorites')->get();
+	$favorites = \DB::table('universities')->join('departments','universities.university_id','=','departments.university_id')->join('subjects','departments.department_id','=','subjects.department_id')
+	->join('policies','subjects.subject_id','=','policies.subject_id')->join('favorites','policies.subject_id','=','favorites.subject_id')
+	->Where('favorites.user_id',$id)->where('universities.is_deleted',0)->where('departments.is_deleted',0)->where('subjects.is_deleted',0)
+	->get();
 
-	return response()->json(['universities' => $universities,'favorites' => $favorites]);
+	return response()->json(['favorites' => $favorites]);
 	});
 
-	Route::get("/read/{id}", function($id){
-		$read = \DB::table('favorites')->where("user_id",$id)->get();
-		return response()->json(['read' => $read]);
-	   });
