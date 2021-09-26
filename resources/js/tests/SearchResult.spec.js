beadda2280a73fa623eb0b24a0a04ccd946e705e
Hiro101998 <asameshi0319@gmail.com>
2021-09-26 13:30:27 +0900

    addfavoriteが機能するかのテスト作成

--- resources/js/tests/SearchResult.spec.js
+++ resources/js/tests/SearchResult.spec.js
@@ -1,24 +1,70 @@
 require("jsdom-global");
-import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+import { shallowMount,createLocalVue } from "@vue/test-utils";
 import Vue from "vue";
 import Vuetify from "vuetify";
 import SearchResult from "../views/SearchResult.vue";
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import flushPromises from "flush-promises";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 
 Vue.use(Vuetify);
 
-describe("methodsのテスト", () => {
+describe("ファイル中でvuetifyを読み込む処理", () => {
     const localVue = createLocalVue();
     let vuetify;
-  
+
     beforeEach(() => {
         vuetify = new Vuetify();
     });
-    //apiでgetする値のモック化
-    it("addFavoriteが機能するか", async () => {
-        const wrapper = mount(SearchResult, {
+
+    describe("検索結果の有無による分岐が機能しているか",() =>{
+        it("検索結果がない時はerrorMessageがtureになる", () => {
+            const wrapper = shallowMount(SearchResult, {
+                // //searchResultの中身が空の時
+                propsData: {
+                    searchResults: []
+                }
+            });
+            //errorMessageが出る
+            expect(wrapper.vm.errorMessage).toBe(true);
+        });
+    
+        it("検索結果がある時はerrorMessageがfalseのまま", () => {
+            const wrapper = shallowMount(SearchResult, {
+                localVue,
+                vuetify,
+                propsData: {
+                    searchResults: {
+                        0: {
+                            //ダミーデータを作成
+                            created_at: "2021-07-15 20:00:00",
+                            department_id: 1,
+                            department_name: "ダミー学部",
+                            field_id_1: 1,
+                            field_id_2: 2,
+                            field_id_3: 3,
+                            field_id_4: 4,
+                            is_deleted: 0,
+                            major_id: 1,
+                            operator_id: 1,
+                            prefecture_id: 1,
+                            region_id: 1,
+                            subject_id: 6,
+                            subject_name: "ダミー学科",
+                            university_id: 6,
+                            university_name: "ダミー大学",
+                            updated_at: "2021-07-15 20:00:00"
+                        }
+                    }
+                }
+            });
+            //検索結果が存在するため、エラーメッセージは出ない。
+            expect(wrapper.vm.errorMessage).toBe(false);
+        });
+    })
+    
+    describe("お気に入り追加ボタンを押した時の処理が正常に機能するか",() =>{
+        //共通で使うダミーデータの設定
+        const wrapper = shallowMount(SearchResult, {
             localVue,
             vuetify,
             propsData: {
@@ -46,107 +92,77 @@ describe("methodsのテスト", () => {
                 }
             }
         });
-        //window.alertのモック化
-        window.alert = jest.fn();
-        //axios.getをモック化
-        let mockAxios = new MockAdapter(axios);
-        mockAxios.onGet('api/favorite/6').reply(200, {
-         favorite_id: 1,
-         user_id:6,
-         subject_id:1,
+        // it("ゲストユーザを判定してお気に入り機能を制限できるか",async() =>{
+        //     global.window = Object.create(window);
+        //  Object.defineProperty(window, "Laravel", {
+        //      value:1,
+        //      configurable: true
+        //  });
+        //     console.log(window.Laravel)
+        //     await wrapper.vm.addFavorite();
+        //     expect(wrapper.vm.checkAuthority).toBe('')
+        //    })
+       
+        describe("お気に入り追加時の重複チェックが機能するか",()=>{
+            //ログインユーザのidを1以外にする。
+         global.window = Object.create(window);
+         Object.defineProperty(window, "Laravel", {
+             value:6,
+             configurable: true
          });
-
+         //window.alertのモック化
+         window.alert = jest.fn();
+         //axios.getをモック化
+         let mockAxios = new MockAdapter(axios);
+         mockAxios.onGet("api/favorite/6").reply(200, {
+             favorites: {
+                 favorite_id: 1,
+                 user_id: 6,
+                 subject_id: 6
+             }
+         });
          //axios.postのモック化
-         mockAxios.onPost('api/store').reply(200, {
-            newFavorite: {
-                subject_id: 1,
-                user_id: 6
-            },
-            });
- 
-         axios.get("api/favorite/6").then(function (response) {
-             console.log(response.data);
-           })
-           .catch(err => {
-             console.log(err);
+         mockAxios.onPost("api/store").reply(200, {
+             newFavorite: {
+                 subject_id: 6,
+                 user_id: 6
+             }
          });
+         axios
+             .get("api/favorite/6")
+             .then(function(response) {})
+             .catch(err => {
+                 console.log(err);
+             });
 
-        await wrapper.setData({
-            user_id :6,
-            newFavorite: {
-                subject_id: 1,
-                user_id: 6
-            },
-            registeredSubject_id: [1,2,3]
-        });
+             it("お気に入り重複がない時にお気に入りに追加されるか", async () => {
+                await wrapper.setData({
+                    clickId: 6,
+                    newFavorite: {
+                        subject_id: 6,
+                        user_id: 6
+                    },
+                    registeredSubject_id: [1],
+                });
+                await wrapper.vm.addFavorite();
+                expect(wrapper.vm.addFavoriteResult).toBe(true);
+            });
+        
+            it("お気に入り重複時にはじかれること", async () => {
+                await wrapper.setData({
+                    clickId: 6,
+                    newFavorite: {
+                        subject_id: 6,
+                        user_id: 6
+                    },
+                    registeredSubject_id: [6]
+                });
+                await wrapper.vm.addFavorite();
+                expect(wrapper.vm.addFavoriteResult).toBe(false);
+            });
+        })   
+  
+    })
+    
+});
 
-         expect(wrapper.vm.newFavorite.subject_id).toBe(1);
-         expect(wrapper.vm.newFavorite.user_id).toBe(6);
-
-    await wrapper.vm.addFavorite();
-    expect(wrapper.vm.addFavoriteResult).toBe(false);
-     })
-    });
-
-// it('検索結果がない時はエラーが表示される', () => {
-//   const wrapper = shallowMount(SearchResult,{
-//   // //searchResultの中身が空の時
-//   propsData:{
-//     searchResults:Array[0]
-//   }
-//   })
-//   // //エラーが描画される
-//   expect(wrapper.text()).toBe('条件に合致するデータがありません。条件を変更して再検索してください。')
-//   })
-
-// it('検索結果がある時はエラーが表示されない', () => {
-//   const wrapper = shallowMount(SearchResult,{
-//   propsData:{
-//     searchResults:{
-//     0:{
-//     //ダミーデータを作成
-//     created_at:"2021-07-15 20:00:00",
-//       department_id:280,
-//       department_name:"外国語学部",
-//       field_id_1:2,
-//       field_id_2:6,
-//       field_id_3:0,
-//       field_id_4:0,
-//       is_deleted:0,
-//       major_id:696,
-//       operator_id:1,
-//       prefecture_id:23,
-//       region_id:3,
-//       subject_id:696,
-//       subject_name:"ヨーロッパ学科",
-//       university_id:97,
-//       university_name:"愛知県立大学",
-//       updated_at:"2021-07-15 20:00:00"
-//     }
-//     }
-//     }
-//   })
-//   //エラーが描画されない
-//   expect(wrapper.text().toBe(''))
-//   })
-
-//メソッドのテスト
-// it('addfavorieの検証', async () =>{
-//   jest.mock("axios", () => ({
-//     get: jest.fn(() => Promise.resolve({ data:
-//           {user_id:1,favorite_id:1},
-//     }))
-//   })
-//   );
-//   const wrapper = shallowMount(SearchResult,{
-//   setData:{
-//     newFavorite:{
-//       subject_id:1,
-//       user_id:1
-//   }
-//   }
-//   })
-//   // await wrapper.vm.addFavorite();
-//   const addFavorite = wrapper.find('div')
-//   console.log(addFavorite)
-// })
--- resources/js/views/SearchResult.vue
+++ resources/js/views/SearchResult.vue
@@ -1,177 +1,210 @@
 <template>
-  <v-app>
-    <div v-if="searchResults != ''">
-      <v-row>
-        <v-col v-for="(searchResult) in searchResults" :key="searchResult.index"
-            cols="12"
-            sm="4">
-          <v-hover v-slot:default="{ hover }">
-          <v-card>
-            <v-card-text>
-              <h2>
-                {{searchResult.university_name}}
-              </h2>
-              <h3>{{searchResult.department_name}}</h3>
-              <p>{{searchResult.subject_name}}</p>
-            </v-card-text>
-
-            <v-fade-transition>
-              <v-overlay
-                v-if="hover"
-                absolute
-                color="#036358"
-              >
-                <v-btn
-                @click="moreInfo(searchResult.subject_id)">
-                See more info
-                </v-btn>
-              </v-overlay>
-            </v-fade-transition>
-          </v-card>
-          </v-hover>
-        </v-col>
-      </v-row>
-
-      <!-- ここからダイアログ -->
-      <v-row v-for="display in displays" :key="display.index">
-        <v-col justify="center">
-          <v-dialog
-            v-model="dialog"
-            width="800px"
-          >
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">３ポリシー</span>
-              </v-card-title>
-              <v-card-text>
-                <h2>ディプロマポリシー</h2>
-                <a v-bind:href="`${display.dp_url}`" target="_blank" rel="noopener noreferrer">ディプロマポリシーのリンク</a>
-                <p style="white-space:pre-wrap;">{{display.diploma_policy}}</p>
-                <h2>カリキュラムポリシー</h2>
-                <a v-bind:href="`${display.cp_url}`" target="_blank" rel="noopener noreferrer">カリキュラムポリシーのリンク</a>
-                <p style="white-space:pre-wrap;">{{display.curriculum_policy}}</p>
-                <h2>アドミッションポリシー</h2>
-                <a v-bind:href="`${display.ap_url}`" target="_blank" rel="noopener noreferrer">アドミッションポリシーのリンク</a>
-                <p style="white-space:pre-wrap;">{{display.admission_policy}}</p>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  color="green darken-1"
-                  text
-                  @click="reset()"
+    <v-app>
+         <div v-if="errorMessage">
+            <v-alert colored-border type="warning" elevation="2">
+                条件に合致するデータがありません。条件を変更して再検索してください。
+            </v-alert>
+        </div>
+        <div v-else>
+            <v-row>
+                <v-col
+                    v-for="searchResult in searchResults"
+                    :key="searchResult.index"
+                    cols="12"
+                    sm="4"
                 >
-                  閉じる
-                </v-btn>
-                <v-btn
-                  class="mx-2 test"
-                  fab
-                  dark
-                  small
-                  color="pink"
-                  @click="addFavorite()"
-                >
-                  <v-icon dark>
-                    mdi-heart
-                  </v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-col>
-      </v-row>
-    </div>
-    <div v-else>
-      <v-alert
-        colored-border
-        type="warning"
-        elevation="2"
-      >
-        条件に合致するデータがありません。条件を変更して再検索してください。
-      </v-alert>
-    </div>
-  </v-app>
+                    <v-hover v-slot:default="{ hover }">
+                        <v-card>
+                            <v-card-text>
+                                <h2>
+                                    {{ searchResult.university_name }}
+                                </h2>
+                                <h3>{{ searchResult.department_name }}</h3>
+                                <p>{{ searchResult.subject_name }}</p>
+                            </v-card-text>
+
+                            <v-fade-transition>
+                                <v-overlay
+                                    v-if="hover"
+                                    absolute
+                                    color="#036358"
+                                >
+                                    <v-btn
+                                        @click="
+                                            moreInfo(searchResult.subject_id)
+                                        "
+                                    >
+                                        See more info
+                                    </v-btn>
+                                </v-overlay>
+                            </v-fade-transition>
+                        </v-card>
+                    </v-hover>
+                </v-col>
+            </v-row>
+
+            <!-- ここからダイアログ -->
+            <v-row v-for="display in displays" :key="display.index">
+                <v-col justify="center">
+                    <v-dialog v-model="dialog" width="800px">
+                        <v-card>
+                            <v-card-title>
+                                <span class="text-h5">３ポリシー</span>
+                            </v-card-title>
+                            <v-card-text>
+                                <h2>ディプロマポリシー</h2>
+                                <a
+                                    v-bind:href="`${display.dp_url}`"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    >ディプロマポリシーのリンク</a
+                                >
+                                <p style="white-space:pre-line;">
+                                    {{ display.diploma_policy }}
+                                </p>
+                                <h2>カリキュラムポリシー</h2>
+                                <a
+                                    v-bind:href="`${display.cp_url}`"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    >カリキュラムポリシーのリンク</a
+                                >
+                                <p style="white-space:pre-line;">
+                                    {{ display.curriculum_policy }}
+                                </p>
+                                <h2>アドミッションポリシー</h2>
+                                <a
+                                    v-bind:href="`${display.ap_url}`"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    >アドミッションポリシーのリンク</a
+                                >
+                                <p style="white-space:pre-line;">
+                                    {{ display.admission_policy }}
+                                </p>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn
+                                    color="green darken-1"
+                                    text
+                                    @click="reset()"
+                                >
+                                    閉じる
+                                </v-btn>
+                                <v-btn
+                                    class="mx-2"
+                                    fab
+                                    dark
+                                    small
+                                    color="pink"
+                                    @click="addFavorite"
+                                >
+                                    <v-icon dark>
+                                        mdi-heart
+                                    </v-icon>
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </v-col>
+            </v-row>
+        </div>
+       
+    </v-app>
 </template>
 
 <script>
- import axios from 'axios'
-//main.blade.phpから,authで取得したデータを持ってくる
+//  window.location.href = 'main#/universitySearch'
+import axios from 'axios'
+const user_id = window.Laravel;
 export default {
-  name:"searchResult",
-  props: ["searchResults"],
-  data(){
-    return{
-      user_id:'',
-      clickId:'',
-      dialog: false,
-      displays:[],
-      favoriteSubject_id:[],
-      registeredSubject_id:[],
-      addFavoriteResult:'',
-
-      newFavorite:{
-        user_id:'',
-        subject_id:'',
-      }
-      
-    }
-  },
-    mounted(){
-      this.user_id = window.laravel
+    props: ["searchResults"],
+    data() {
+        return {
+            errorMessage:false,
+            clickId: "",
+            dialog: false,
+            displays: [],
+            favorites: [],
+            favoriteSubject_id: [],
+            registeredSubject_id: [],
+            checkAuthority:true,
+            addFavoriteResult:'',
+            newFavorite: {
+                user_id: "",
+                subject_id: ""
+            }
+        };
     },
-      methods: {
+    mounted(){
+        if(this.searchResults.length <1){
+            this.errorMessage = true
+        }
+    },
+    methods: {
         moreInfo(getSubjectId) {
-        this.clickId = getSubjectId
-        const getPolicyUrl = "/api/policy/" + getSubjectId
-	      axios.get(getPolicyUrl)
-		    .then(response => {
-        this. displays = response.data.policies;
-        })
-		    .catch(error => {
-		    console.log(error)
-			  });
-				this.dialog =true
+            this.clickId = getSubjectId;
+            const getPolicyUrl = "/api/policy/" + getSubjectId;
+            axios
+                .get(getPolicyUrl)
+                .then(response => {
+                    this.displays = response.data.policies;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            this.dialog = true;
         },
-        reset(){
-          this.clickId ='',
-          this.displays =''
-          this.dialog = false
+        reset() {
+            (this.clickId = ""), (this.displays = "");
+            this.dialog = false;
         },
         //お気に入りに追加する
-          addFavorite(){
-          this.newFavorite.subject_id = this.clickId
-          this.newFavorite.user_id = this.user_id
-          //重複チェック
-          const getFavoriteUrl = '/api/favorite/' + this.user_id
-          axios.get(getFavoriteUrl)
-          .then(response => {
-          //お気に入りデータの取得
-          for( let i =0;i<response.data.favorites;i++){
-						this.registeredSubject_id.push(response.data.favorites[i].subject_id);
-			        }
-          if(this.registeredSubject_id.includes(this.newFavorite.subject_id)){
-                alert("お気に入りに登録済です。");
-                this.addFavoriteResult = false
-              }
-           else{
-            let favorite = this.newFavorite;
-            axios.post('/api/store', favorite)
-            .then(response => {
-              // favoite関係のデータをリセットする。
-              this.registeredSubject_id =[]
-            })
-             .catch(error => {
-              console.log(error)
-            }); 
-             alert("お気に入りに追加しました。");
-              this.addFavoriteResult = true
-              this.dialog =false
-          }     
-          })
-          .catch(error => {
-          console.log(error)
-          }); 
+        async addFavorite() {
+            //ゲストユーザーはお気に入り
+            if (user_id == 1) {
+                alert("ゲストユーザーはお気に入り機能を利用できません");
+                this.checkAuthority = false
+            } else {
+                this.newFavorite.subject_id = this.clickId;
+                this.newFavorite.user_id = user_id;
+                //重複チェック
+                const getFavoriteUrl = "/api/favorite/" + window.Laravel;
+                await axios
+                    .get(getFavoriteUrl)
+                    .then(response => {
+                        //お気に入りデータの取得
+                        let favorites = response.data.favorites;
+                        for (let i = 0; i < favorites.length; i++) {
+                            this.registeredSubject_id.push(
+                                favorites[i].subject_id
+                            );
+                        }
+                        if (
+                            this.registeredSubject_id.includes(
+                                this.newFavorite.subject_id
+                            )
+                        ) {
+                            alert("お気に入りに登録済です。");
+                            this.addFavoriteResult =false;
+                        } else {
+                            let favorite = this.newFavorite;
+                            axios.post("/api/store", favorite).then(res => {
+                                // favoite関係のデータをリセットする。
+                                this.favorites = [];
+                                this.registeredSubject_id = [];
+                            });
+                            alert("お気に入りに追加しました。");
+                            this.dialog = false;
+                            this.addFavoriteResult =true
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            }
         }
-  },
-}
+    }
+};
 </script>
